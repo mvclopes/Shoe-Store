e4f6713734c27f8cc11b33cc88e5b04382090837
Matheus Vinícius Carvalho Lopes <malopes@gertec.local>
2021-06-15 17:14:16 -0300

    Screen Welcome

---
+++ app/src/main/java/com/udacity/shoestore/view/welcome/WelcomeFragment.kt
@@ -0,0 +1,47 @@
+package com.udacity.shoestore.view.welcome
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+import timber.log.Timber
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+    private lateinit var viewModel: WelcomeViewModel
+    private lateinit var viewModelFactory: WelcomeViewModelFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val welcomeFragmentArgs by navArgs<WelcomeFragmentArgs>()
+        viewModelFactory = WelcomeViewModelFactory(welcomeFragmentArgs.userName)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(WelcomeViewModel::class.java)
+
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        binding.fabInstructions.setOnClickListener{
+            //TODO: Fazer navegação para tela de instruções
+            Toast.makeText(context, "We're going to instruction screen! Are you ready?!", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/udacity/shoestore/view/welcome/WelcomeViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.udacity.shoestore.view.welcome
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+
+class WelcomeViewModelFactory(private val userName: String): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WelcomeViewModel::class.java)){
+            return WelcomeViewModel(userName) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+
+}
